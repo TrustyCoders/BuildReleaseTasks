23b2070f83fbea48afbeb86beede13d8cb064e02
bojan.zlatic <bojan.zlatic@mediportal.com>
2021-07-20 17:06:36 +0200

    get youngest cert

--- GetYoungestCertForHostHeaderTask/GetYoungestCertForHostHeader/GetYoungestCertForHostHeader.ps1
+++ GetYoungestCertForHostHeaderTask/GetYoungestCertForHostHeader/GetYoungestCertForHostHeader.ps1
@@ -1,52 +1,41 @@
-Write-Output "Start searching for the youngest certs"
+Write-Output "Start searching for the youngest cert"
 
-[string]$HostHeaders = Get-VstsInput -Name HostHeader -Require
+[string]$HostHeader = Get-VstsInput -Name HostHeader -Require
 [string]$FriendlyName = Get-VstsInput -Name FriendlyName -Require
 [string]$Resultname = Get-VstsInput -Name ResultName
 
-$youngestCertsThumbprints= @{}
+$youngestCert = $null
+$certAge = [System.DateTime]::MinValue
+$dateTimeMinValue=[System.DateTime]::MinValue
 $certs = Get-ChildItem -Path Cert:\LocalMachine\My
+$youngestCertThumbprint = $null
 
-if ($HostHeaders -eq $null){
-    $HostHeaders='';
-}
+if ($HostHeader -ne $null -and $HostHeader -ne ""){
+    $hostHeader=$HostHeader.Trim()
 
-$HostHeaderArray = $HostHeaders.Split(",")
-if ($HostHeaderArray.Count -gt 0){
-    $HostHeaderArray | ForEach-Object {
-        $hostHeader=$_.Trim()
-        $youngestCert = $null
-        $certAge = [System.DateTime]::MinValue
-        $dateTimeMinValue=[System.DateTime]::MinValue
-
-        foreach($singleCert in $certs)
+    foreach($singleCert in $certs)
+    {
+        $allExtensions =  $singleCert.Extensions
+        foreach($singleExtension in $allExtensions)
         {
-            $allExtensions =  $singleCert.Extensions
-            foreach($singleExtension in $allExtensions)
+            if($singleExtension.Oid.FriendlyName -eq $FriendlyName)
             {
-                if($singleExtension.Oid.FriendlyName -eq $FriendlyName)
+                if($singleExtension.Format(1).Contains("DNS Name=$hostHeader") -or $singleCert.Subject.Contains("CN=$hostHeader"))
                 {
-                    if($singleExtension.Format(1).Contains("DNS Name=$hostHeader") -or $singleCert.Subject.Contains("CN=$hostHeader"))
+                    If($certAge -lt $singleCert.NotBefore -or $certAge -eq $dateTimeMinValue)
                     {
-                        If($certAge -lt $singleCert.NotBefore -or $certAge -eq $dateTimeMinValue)
-                        {
-                            $youngestCert=$singleCert
-                            $certAge = $singleCert.NotBefore
-                        }
+                        $youngestCert=$singleCert
+                        $certAge = $singleCert.NotBefore
                     }
                 }
             }
         }
-        $youngestCertsThumbprints+= @{$hostHeader = $youngestCert.Thumbprint}
-        Write-Output "HostHeader: $hostHeader"
-        $youngestCertThumbprint=$youngestCert.Thumbprint
-        Write-Output "Thumbprint: $youngestCertThumbprint"
     }
+    Write-Output "HostHeader: $hostHeader"
+    $youngestCertThumbprint=$youngestCert.Thumbprint
+    Write-Output "Thumbprint: $youngestCertThumbprint"
 }
 
-
-$youngestCertsThumbprintsJson = $youngestCertsThumbprints | ConvertTo-Json
-
-Write-Host "##vso[task.setvariable variable=$ResultName]$youngestCertsThumbprintsJson"
+Write-Host "##vso[task.setvariable variable=$ResultName]$youngestCertThumbprint"
 
     
